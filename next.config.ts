4a293e9d91333a55ca146661a7bf52122fab0136
yoshifoxor <yshs.adachi@gmail.com>
2025-11-09 05:01:58 +0900

    feat: update Next.js configuration and modify font settings

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+  // reactCompiler: true,
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
